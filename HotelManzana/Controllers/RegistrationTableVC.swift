c1bb3ddd94c05decff8d4863fd53e4d9f3c00306
steph <>
2017-12-13 14:30:45 +0200

    arrange the cells and rows

---
+++ HotelManzana/Controllers/RegistrationTableVC.swift
@@ -0,0 +1,25 @@
+//
+//  RegistrationTableVC.swift
+//  HotelManzana
+//
+//  Created by steph on 13/12/2017.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationTableVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+   
+
+}
